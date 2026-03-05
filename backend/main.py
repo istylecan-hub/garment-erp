42ff0b34ac538f302cae0355e92cfdc5d3c5f9fc
Akhilesh Yadav <istylecan@gmail.com>
2026-03-05 19:37:40 +0530

    test server

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-from database import engine
-from models import Base
 
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine)
-
 @app.get("/")
 def root():
-    return {"message": "Garment ERP Backend Running"}
+    return {"status": "running"}
